29a97cbea0a0846f496606c22a56fc153b016ae1
Daniel Ebert <daniel.ebert@protonmail.com>
2021-05-15 13:37:59 +0200

    Updated DSA_DATA_NEWEST_VERSION Now the auto update actually works

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ use std::fs;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
-const DSA_DATA_NEWEST_VERSION: u64 = 1;
+const DSA_DATA_NEWEST_VERSION: u64 = 2;
 
 mod default {
     pub fn auto_update_dsa_data() -> bool {
